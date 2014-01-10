38d1380ee0f529019eb440760e5d1d2228cd0d87
George Trifonov <gtrifonov@gmail.com>
2014-01-10 13:17:54 -0800

    Addressing code review feedback

--- test/README.md
+++ test/README.md
@@ -1,14 +1,17 @@
-<h1>Windows Azure Media Services Verification Tests</h1>
-<p>This set of test allows you to verify Windows Azure Media Services .Net SDK functionality using your existing Windows Azure Media Services account</p>
+<h1>Windows Azure Media Services Tests</h1>
+<p>Windows Azure Media Services Client .NET SDK contains two sets of test which will help developers to verify source code.
+<H2>Scenario tests  located in azure-sdk-for-media-services\test\net\Scenario</H2>
+
+This set of test allows you to verify end to end scenarios using your existing Windows Azure Media Services account </p>
 <p>In order to execute test you need to modify app.config file and provide following information</p>
 
-<pre><code>
-
-&lt;add key="ClientSecret"  value="PUT_YOUR_ACCOUNT_PRIMARY_KEY_HERE"/&gt;
-&lt;add key="ClientId" value="PUT_YOUR_ACCOUNT_NAME_HERE"/&gt;
-</code>
+<pre><code>&lt;add key="ClientSecret"  value="PUT_YOUR_ACCOUNT_PRIMARY_KEY_HERE"/&gt;
+&lt;add key="ClientId" value="PUT_YOUR_ACCOUNT_NAME_HERE"/&gt;</code>
 </pre>
 
 <p><b>Please note</b><br>
 Executing test will generate encoding workload in your account which will be charged according to  Windows Azure Media Services pricing model for your account <a href="http://www.windowsazure.com/en-us/pricing/details/media-services/">
 http://www.windowsazure.com/en-us/pricing/details/media-services/</a> </p>
+
+<H2>Unit Tests located in azure-sdk-for-media-services\test\net\unit</H2>
+Unit test project contains tests which doesn't have requirements to be executed against your production account and will verify functionality of client sdk.
--- test/net/unit/AssetUnitTest.cs
+++ test/net/unit/AssetUnitTest.cs
@@ -18,10 +18,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.WindowsAzure.MediaServices.Client.Tests.Common;
+using Moq;
 
 namespace Microsoft.WindowsAzure.MediaServices.Client.Tests.UnitTests
 {
@@ -77,6 +79,147 @@ namespace Microsoft.WindowsAzure.MediaServices.Client.Tests.UnitTests
             Assert.IsNotNull(asset);
         }
 
+        #region Retry Logic tests
+
+        [TestMethod]
+        [Priority(0)]
+        [TestCategory("DailyBvtRun")]
+        public void TestAssetCreateRetry()
+        {
+            var expected = new AssetData { Name = "testData" };
+            var fakeException = new WebException("test", WebExceptionStatus.ConnectionClosed);
+            var dataContextMock = TestMediaServicesClassFactory.CreateSaveChangesMock(fakeException, 2, expected);
+
+            dataContextMock.Setup((ctxt) => ctxt.AddObject("Assets", It.IsAny<object>()));
+
+            _mediaContext.MediaServicesClassFactory = new TestMediaServicesClassFactory(dataContextMock.Object);
+
+            IAsset asset = _mediaContext.Assets.Create("Empty", "some storage", AssetCreationOptions.None);
+            Assert.AreEqual(expected.Name, asset.Name);
+            dataContextMock.Verify((ctxt) => ctxt.SaveChangesAsync(It.IsAny<object>()), Times.Exactly(2));
+        }
+
+        [TestMethod]
+        [Priority(0)]
+        [TestCategory("DailyBvtRun")]
+        [ExpectedException(typeof(WebException))]
+        public void TestAssetCreateFailedRetry()
+        {
+            var expected = new AssetData { Name = "testData" };
+            var fakeException = new WebException("test", WebExceptionStatus.ConnectionClosed);
+            var dataContextMock = TestMediaServicesClassFactory.CreateSaveChangesMock(fakeException, 10, expected);
+
+            dataContextMock.Setup((ctxt) => ctxt.AddObject("Assets", It.IsAny<object>()));
+
+            _mediaContext.MediaServicesClassFactory = new TestMediaServicesClassFactory(dataContextMock.Object);
+
+            try
+            {
+                IAsset asset = _mediaContext.Assets.Create("Empty", "some storage", AssetCreationOptions.None);
+            }
+            catch (WebException x)
+            {
+                dataContextMock.Verify((ctxt) => ctxt.SaveChangesAsync(It.IsAny<object>()), Times.AtLeast(3));
+                Assert.AreEqual(fakeException, x);
+                throw;
+            }
+
+            Assert.Fail("Expected exception");
+        }
+
+        [TestMethod]
+        [Priority(0)]
+        [TestCategory("DailyBvtRun")]
+        [ExpectedException(typeof(WebException))]
+        public void TestAssetCreateFailedRetryMessageLengthLimitExceeded()
+        {
+            var expected = new AssetData { Name = "testData" };
+
+            var fakeException = new WebException("test", WebExceptionStatus.MessageLengthLimitExceeded);
+
+            var dataContextMock = TestMediaServicesClassFactory.CreateSaveChangesMock(fakeException, 10, expected);
+
+            dataContextMock.Setup((ctxt) => ctxt.AddObject("Assets", It.IsAny<object>()));
+
+            _mediaContext.MediaServicesClassFactory = new TestMediaServicesClassFactory(dataContextMock.Object);
+
+            try
+            {
+                IAsset asset = _mediaContext.Assets.Create("Empty", "some storage", AssetCreationOptions.None);
+            }
+            catch (WebException x)
+            {
+                dataContextMock.Verify((ctxt) => ctxt.SaveChangesAsync(It.IsAny<object>()), Times.Exactly(1));
+                Assert.AreEqual(fakeException, x);
+                throw;
+            }
+
+            Assert.Fail("Expected exception");
+        }
+
+        [TestMethod]
+        [Priority(0)]
+        [TestCategory("DailyBvtRun")]
+        public void TestAssetUpdateRetry()
+        {
+            var data = new AssetData { Name = "testData" };
+            var fakeException = new WebException("test", WebExceptionStatus.ConnectionClosed);
+            var dataContextMock = TestMediaServicesClassFactory.CreateSaveChangesMock(fakeException, 2, data);
+
+            dataContextMock.Setup((ctxt) => ctxt.AttachTo("Assets", data));
+            dataContextMock.Setup((ctxt) => ctxt.UpdateObject(data));
+
+            _mediaContext.MediaServicesClassFactory = new TestMediaServicesClassFactory(dataContextMock.Object);
+
+            data.SetMediaContext(_mediaContext);
+
+            data.Update();
+
+            dataContextMock.Verify((ctxt) => ctxt.SaveChangesAsync(data), Times.Exactly(2));
+        }
+
+        [TestMethod]
+        [Priority(0)]
+        [TestCategory("DailyBvtRun")]
+        public void TestAssetDeleteRetry()
+        {
+            var data = new AssetData { Name = "testData" };
+
+            var fakeException = new WebException("test", WebExceptionStatus.ConnectionClosed);
+
+            var dataContextMock = TestMediaServicesClassFactory.CreateSaveChangesMock(fakeException, 2, data);
+
+            dataContextMock.Setup((ctxt) => ctxt.AttachTo("Assets", data));
+            dataContextMock.Setup((ctxt) => ctxt.DeleteObject(data));
+
+            _mediaContext.MediaServicesClassFactory = new TestMediaServicesClassFactory(dataContextMock.Object);
+
+            data.SetMediaContext(_mediaContext);
+
+            data.Delete();
+
+            dataContextMock.Verify((ctxt) => ctxt.SaveChangesAsync(data), Times.Exactly(2));
+        }
+
+        [TestMethod]
+        [Priority(0)]
+        [TestCategory("DailyBvtRun")]
+        public void TestAssetGetContentKeysRetry()
+        {
+            var data = new AssetData { Name = "testData", Id = "testId" };
+
+            var dataContextMock = TestMediaServicesClassFactory.CreateLoadPropertyMockConnectionClosed(2, data);
+
+            _mediaContext.MediaServicesClassFactory = new TestMediaServicesClassFactory(dataContextMock.Object);
+
+            data.SetMediaContext(_mediaContext);
+
+            var keys = ((IAsset)data).ContentKeys;
+
+            dataContextMock.Verify((ctxt) => ctxt.LoadProperty(data, "ContentKeys"), Times.Exactly(2));
+        }
+
+        #endregion Retry Logic tests
        
     }
 }
