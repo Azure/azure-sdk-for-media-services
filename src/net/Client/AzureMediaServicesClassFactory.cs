f38e2f68a1dc6401dcc29ce5a4d917d321e94393
Ivan Shaporov <ivan_shaporov@yahoo.com>
2014-01-14 14:33:41 -0800

    Use account name and azureMediaServicesEndpoint in the key for caching account API endpoint.

--- src/net/Client/AzureMediaServicesClassFactory.cs
+++ src/net/Client/AzureMediaServicesClassFactory.cs
@@ -58,8 +58,13 @@ namespace Microsoft.WindowsAzure.MediaServices.Client
             this._serviceVersionAdapter = serviceVersionAdapter;
             this._mediaContext = mediaContext;
 
+            string cacheKey = string.Format(
+                "{0},{1}",
+                mediaContext.Credentials.ClientId,
+                azureMediaServicesEndpoint.ToString());
+
             this._azureMediaServicesEndpoint = _endpointCache.GetOrAdd(
-                azureMediaServicesEndpoint.ToString(),
+                cacheKey,
                 () => GetAccountApiEndpoint(this._dataServiceAdapter, this._serviceVersionAdapter, azureMediaServicesEndpoint),
                 () => mediaContext.Credentials.TokenExpiration);
         }
