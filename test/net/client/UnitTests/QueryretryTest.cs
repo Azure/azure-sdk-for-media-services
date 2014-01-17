13c5a209bbfc7ed27d8b02108d503a3e01e62f54
Ivan Shaporov <ivan_shaporov@yahoo.com>
2014-01-17 13:03:59 -0800

    Address feedback comments.

--- test/net/client/UnitTests/QueryretryTest.cs
+++ test/net/client/UnitTests/QueryretryTest.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="QueryRetryTest.cs" company="Microsoft">Copyright 2012 Microsoft Corporation</copyright>
+// <license>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </license>
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -48,7 +64,7 @@ namespace Microsoft.WindowsAzure.MediaServices.Client.Tests.UnitTests
 		#endregion
 	}
 
-	class TrowingQueryable : IQueryable<string>
+	class ThrowingQueryable : IQueryable<string>
 	{
 		IQueryable<string> _inner = new[] { "first" }.AsQueryable();
 
@@ -100,7 +116,7 @@ namespace Microsoft.WindowsAzure.MediaServices.Client.Tests.UnitTests
 		{
 			MediaRetryPolicy queryRetryPolicy = new TestMediaServicesClassFactory(null).GetQueryRetryPolicy();
 
-			var mock = new TrowingQueryable();
+			var mock = new ThrowingQueryable();
 
 			var target = new MediaQueryable<string, string>(mock, queryRetryPolicy);
 			Assert.AreEqual(mock.Inner.First(), target.First());
